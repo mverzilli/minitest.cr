c7eec9066c055c77ce100ce86e26e67aea63d682
Julien Portalier <julien@portalier.com>
2017-04-19 16:56:18 +0200

    Collect and run tests globally (not per test suite)

--- src/runnable.cr
+++ src/runnable.cr
@@ -6,17 +6,31 @@ module Minitest
       {% end %}
     end
 
-    macro def self.run(reporter)
-      {{ @type }}.run_tests(reporter)
+    alias Data = {Test.class, String, Proc(Test, Nil)}
+    @@tests = [] of Data
+
+    def self.tests
+      @@tests
+    end
+
+    # Builds, at compile time, an Array with the test method name (String) and a
+    # proc to call that method. The Array will then be shuffled at runtime.
+    macro def self.collect_tests
+      {% begin %}
+        {% for name in @type.methods.map(&.name).select(&.starts_with?("test_")) %}
+          %proc = ->(test : Test) {
+            test.as({{ @type }}).{{ name }}
+            nil
+          }
+          Runnable.tests << { {{ @type }}, {{ name.stringify }}, %proc }
+        {% end %}
+      {% end %}
       nil
     end
 
     getter reporter : AbstractReporter
 
     def initialize(@reporter)
-    end
-
-    def self.run_tests(reporter)
     end
   end
 end
